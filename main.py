7874b0070d6e92c7b5247bdfdcf76f3601cd03b9
Brandon <pokemonbrandon1234@gmail.com>
2021-12-30 09:07:28 +0000

    Added mean mode whatever stuff.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import mean, max, min, variance
 
 
 def main():
     # Load pyspark dataframe
     spark = SparkSession.builder.getOrCreate()
     df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)
-    df.show()
-    df.printSchema()
+
+    df.groupby("Status").count().show()
 
 
 main()
