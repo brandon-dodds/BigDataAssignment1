d9e967e3d61130a3fde09c14cf209ae79213d96a
Brandon <pokemonbrandon1234@gmail.com>
2021-12-31 11:15:23 +0000

    Summary kinda goated, Get MEAN somewhere...

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import mean, max, min, variance
-
-
-def get_features(dataframe):
-    data_mean = dataframe.select(*[mean(c).alias(c) for c in dataframe.columns[1:]])
-    data_max = dataframe.select(*[max(c).alias(c) for c in dataframe.columns[1:]])
-    data_min = dataframe.select(*[min(c).alias(c) for c in dataframe.columns[1:]])
-    data_variance = dataframe.select(*[variance(c).alias(c) for c in dataframe.columns[1:]])
-    data_median = dataframe.approxQuantile([c for c in dataframe.columns[1:]], [0.5], 0.25)
-    data_mode = [dataframe.groupby(i).count().orderBy("count", ascending=False).first()[0] for i in dataframe.columns]
-    return data_min, data_max, data_mean, data_variance, data_median, data_mode
 
 
 def main():
@@ -20,13 +9,9 @@ def main():
     df_normal = df.filter(df.Status == "Normal")
     df_abnormal = df.filter(df.Status == "Abnormal")
 
-    normal_min, normal_max, normal_mean, normal_variance, normal_median, normal_mode = get_features(df_normal)
-    abnormal_min, abnormal_max, abnormal_mean, abnormal_variance, abnormal_median, normal_mode = \
-        get_features(df_abnormal)
-    # names = df.schema.names
-    #
-    # for name in names[1:]:
-    #     df.filter(df.Status == "Normal").select(mean(name)).show()
+    normal_features = df_normal.summary()
+    abnormal_features = df_abnormal.summary()
+    print(normal_features.show())
 
 
 main()
