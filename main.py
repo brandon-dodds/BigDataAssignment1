7089d970f4f40e514699d97272451e072aff2c7a
Brandon <pokemonbrandon1234@gmail.com>
2022-01-01 09:21:08 +0000

    Got MEAN now NEED MODE.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from pyspark.sql import SparkSession
 
 
+def prepare_summary_statistics(column):
+    table_data = []
+    # column_summary.show()
+    rows = column.summary().collect()
+    col_min, col_max, col_mean, col_median, col_variance = rows[3][1], rows[7][1], rows[1][1], rows[5][1], rows[2][1]
+    table_data.extend([col_min, col_max, col_mean, col_median, col_variance])
+    return table_data
+
+
 def main():
     # Load pyspark dataframe
     spark = SparkSession.builder.getOrCreate()
@@ -9,9 +18,9 @@ def main():
     df_normal = df.filter(df.Status == "Normal")
     df_abnormal = df.filter(df.Status == "Abnormal")
 
-    normal_features = df_normal.summary()
-    abnormal_features = df_abnormal.summary()
-    print(normal_features.show())
+    for c in df.columns[1:]:
+        df_col = df_normal.select(c)
+        prepare_summary_statistics(df_col)
 
 
 main()
