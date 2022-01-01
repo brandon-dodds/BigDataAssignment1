7aedc1c2b6ce80458bf759bea6ad237c972f6675
Brandon <pokemonbrandon1234@gmail.com>
2022-01-01 09:50:28 +0000

    WE GOT MODE I REPEAT WE DID IN FACT GET MODE

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from pyspark.sql import SparkSession
+from statistics import multimode
+
+
+def return_modes(column):
+    column_vals = column.collect()
+    vals = [x[0] for x in column_vals]
+    return multimode(vals)
 
 
 def prepare_summary_statistics(column):
     table_data = []
     # column_summary.show()
+    col_mode = return_modes(column)
     rows = column.summary().collect()
+    return_modes(column)
     col_min, col_max, col_mean, col_median, col_variance = rows[3][1], rows[7][1], rows[1][1], rows[5][1], rows[2][1]
-    table_data.extend([col_min, col_max, col_mean, col_median, col_variance])
+    table_data.extend([col_min, col_max, col_mean, col_mode, col_median, col_variance])
     return table_data
 
 
