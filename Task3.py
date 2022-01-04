f97a2e8f3fd85d1e767157637d7ca1d1b6979215
Brandon <pokemonbrandon1234@gmail.com>
2022-01-04 16:40:28 +0000

    Something outputs ig

--- Task3.py
+++ Task3.py
@@ -2,18 +2,24 @@ from pyspark.sql import SparkSession
 from pyspark.ml.stat import Correlation
 from pyspark.ml.feature import VectorAssembler
 import seaborn as sn
+import matplotlib.pyplot as plt
 
 
 def main():
     spark = SparkSession.builder.getOrCreate()
     df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)
+
+    df_normal = df.filter(df.Status == "Abnormal")
+    # df_abnormal = df.filter(df.Status == "Abnormal")
+
     vector_col = "corr_features"
-    assembler = VectorAssembler(inputCols=df.columns[1:], outputCol=vector_col)
-    df_vector = assembler.transform(df).select(vector_col)
+    assembler = VectorAssembler(inputCols=df_normal.columns[1:], outputCol=vector_col)
+    df_vector = assembler.transform(df_normal).select(vector_col)
 
     # get correlation matrix
     matrix = Correlation.corr(df_vector, vector_col)
-    sn.heatmap(matrix.collect()[0]["pearson({})".format(vector_col)].values, annot=True)
+    sn.heatmap(matrix.collect()[0]["pearson({})".format(vector_col)].values.reshape(12, 12), annot=True)
+    plt.show()
 
 
 main()
