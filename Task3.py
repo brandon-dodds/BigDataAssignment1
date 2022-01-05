32f708c28a2f870ebf00e54e0eea3a496a431c03
Brandon <pokemonbrandon1234@gmail.com>
2022-01-05 15:06:34 +0000

    Matrices look good

--- Task3.py
+++ Task3.py
@@ -5,21 +5,26 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 
 
-def main():
-    spark = SparkSession.builder.getOrCreate()
-    df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)
-
-    df_normal = df.filter(df.Status == "Abnormal")
-    # df_abnormal = df.filter(df.Status == "Abnormal")
-
+def create_matrix(dataset, x_label):
     vector_col = "corr_features"
-    assembler = VectorAssembler(inputCols=df_normal.columns[1:], outputCol=vector_col)
-    df_vector = assembler.transform(df_normal).select(vector_col)
+    assembler = VectorAssembler(inputCols=dataset.columns[1:], outputCol=vector_col)
+    df_vector = assembler.transform(dataset).select(vector_col)
 
     # get correlation matrix
     matrix = Correlation.corr(df_vector, vector_col)
     sn.heatmap(matrix.collect()[0]["pearson({})".format(vector_col)].values.reshape(12, 12), annot=True)
+    plt.xlabel("Normal")
     plt.show()
+
+
+def main():
+    spark = SparkSession.builder.getOrCreate()
+    df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)
+
+    df_normal = df.filter(df.Status == "Normal")
+    df_abnormal = df.filter(df.Status == "Abnormal")
+    create_matrix(df_normal, "Normal")
+    create_matrix(df_abnormal, "Abnormal")
 
 
 main()
