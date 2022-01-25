2a92d605e0b832dc1b18b5d9a180f19dc72eab19
Brandon <pokemonbrandon1234@gmail.com>
2022-01-25 14:22:36 +0000

    Changed it based on Docs

--- Section2.py
+++ Section2.py
@@ -2,20 +2,14 @@ from pyspark.sql import SparkSession
 from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import StringIndexer
 from pyspark.ml.classification import DecisionTreeClassifier
+from pyspark.ml.feature import VectorIndexer
 
 
 def main():
     spark = SparkSession.builder.getOrCreate()
     df = spark.read.csv("nuclear_plants_small_dataset.csv")
-    splits = df.randomSplit([0.7, 0.3])
-    print(splits[0].count())
-    print(splits[1].count())
-    string_indexer = StringIndexer()
-    si_model = string_indexer.fit(splits[0])
-    td = si_model.transform(df)
-    dt = DecisionTreeClassifier(maxDepth=2)
-    model = dt.fit(td)
-    print(model.getLabelCol())
+    feature_indexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4)
+    feature_indexer.fit(df)
 
 
 main()
