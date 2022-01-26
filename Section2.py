43dc8aa266f432bc288274a341c781a210a3c753
Brandon <pokemonbrandon1234@gmail.com>
2022-01-26 13:25:28 +0000

    Clean code more dummy.

--- Section2.py
+++ Section2.py
@@ -7,6 +7,17 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml import Pipeline
 
 
+def predictions(label_indexer, feature_indexer, train_data, test_data, classifier):
+    pipeline = Pipeline(stages=[label_indexer, feature_indexer, classifier])
+    model = pipeline.fit(train_data)
+    return model.transform(test_data)
+
+
+def errors(evaluator, prediction, name):
+    accuracy = evaluator.evaluate(prediction)
+    return f"Test Error {name} = %g " % (1.0 - accuracy)
+
+
 def main():
     spark = SparkSession.builder.getOrCreate()
     df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)
@@ -21,13 +32,16 @@ def main():
     dt = DecisionTreeClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures")
     trainer = MultilayerPerceptronClassifier(maxIter=1000, layers=[4, 5, 4, 3], labelCol="indexedLabel")
     lsvc = LinearSVC(maxIter=1000, labelCol="indexedLabel")
-    pipeline = Pipeline(stages=[label_indexer, feature_indexer, dt])
-    model = pipeline.fit(df_train)
-    predictions = model.transform(df_test)
+
+    prediction_dt = predictions(label_indexer, feature_indexer, df_train, df_test, dt)
+    prediction_trainer = predictions(label_indexer, feature_indexer, df_train, df_test, trainer)
+    prediction_lsvc = predictions(label_indexer, feature_indexer, df_train, df_test, lsvc)
+
     evaluator = MulticlassClassificationEvaluator(
         labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
-    accuracy = evaluator.evaluate(predictions)
-    print("Test Error = %g " % (1.0 - accuracy))
+    print(errors(evaluator, prediction_dt, "Decision Tree"))
+    print(errors(evaluator, prediction_trainer, "Multilayer Perceptron"))
+    print(errors(evaluator, prediction_lsvc, "Support Vector Machine"))
 
 
 main()
