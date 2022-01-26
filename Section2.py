a0706f26a8dd6758f46b1ea2dd95c14bc6c68626
Brandon <pokemonbrandon1234@gmail.com>
2022-01-26 10:18:06 +0000

    word!

--- Section2.py
+++ Section2.py
@@ -10,14 +10,17 @@ from pyspark.ml import Pipeline
 def main():
     spark = SparkSession.builder.getOrCreate()
     df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)
-    label_indexer = StringIndexer(inputCol=df.columns[0], outputCol='indexedLabel').fit(df)
-    feature_assembler = VectorAssembler(inputCols=df.columns[1:],
-                                        outputCol='assembledFeatures').transform(df)
-    feature_indexer = VectorIndexer(inputCol='assembledFeatures', outputCol='indexedFeatures', maxCategories=4).fit(df)
-    (trainingData, testData) = df.randomSplit([0.7, 0.3])
-    dt = DecisionTreeClassifier(labelCol='indexedLabel', featuresCol='indexedFeatures')
+    feature_assembler = VectorAssembler(inputCols=df.columns[1:], outputCol="features")
+    feature_data = feature_assembler.transform(df)
+
+    df_train, df_test = feature_data.randomSplit([0.7, 0.3])
+
+    label_indexer = StringIndexer(inputCol=df.columns[0], outputCol="indexedLabel").fit(feature_data)
+    feature_indexer = \
+        VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=2).fit(feature_data)
+    dt = DecisionTreeClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures")
     pipeline = Pipeline(stages=[label_indexer, feature_indexer, dt])
-    model = pipeline.fit(trainingData)
+    model = pipeline.fit(df_train)
 
 
 main()
