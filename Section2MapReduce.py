4d0e65acd14a0892de4a8054a1114db7fefc3e40
Computing <computers@computing.com>
2022-01-29 09:12:24 +0000

    Added more files and read csv

--- Section2MapReduce.py
+++ Section2MapReduce.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+from pyspark.sql import SparkSession
+
+
+def main():
+    spark = SparkSession.builder.getOrCreate()
+    df = spark.read.csv("nuclear_plants_big_dataset.csv", inferSchema=True, header=True)
+    df.show()
+
+
+main()
